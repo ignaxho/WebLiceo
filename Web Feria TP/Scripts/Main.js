ea00f583c08df83203de39a5795c2ca77f66c256
Ignaxhiop <88006411+ignaxho@users.noreply.github.com>
2025-07-27 16:43:02 -0400

    Delete Web Feria TP directory

--- Web Feria TP/Scripts/Main.js
+++
@@ -1,7 +0,0 @@
-// Script principal que coordina otros scripts
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Documento cargado y listo');
-    
-    // Aquí puedes inicializar otros componentes
-    // o manejar dependencias entre scripts
-});
--- Web Feria TP/Scripts/Redirect.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('codigoR').addEventListener('click', function() {
-
-    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=L4h15YkNTz8pnWsC";
-});
